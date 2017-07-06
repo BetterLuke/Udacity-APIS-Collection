090b6cc791606dffdf9557dfdc30b8b55b39c9fa
luke <luke.bei.2015@gmail.com>
2017-07-06 16:08:32 +0800

    [format] beautiful style

--- Lesson_4/12_Rate Limiting/views.py
+++ Lesson_4/12_Rate Limiting/views.py
@@ -4,12 +4,9 @@ redis = Redis()
 import time
 from functools import update_wrapper
 from flask import request, g
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-
-
-
 
 
 class RateLimit(object):
@@ -29,11 +26,14 @@ class RateLimit(object):
     remaining = property(lambda x: x.limit - x.current)
     over_limit = property(lambda x: x.current >= x.limit)
 
+
 def get_view_rate_limit():
     return getattr(g, '_view_rate_limit', None)
 
+
 def on_over_limit(limit):
-    return (jsonify({'data':'You hit the rate limit','error':'429'}),429)
+    return (jsonify({'data': 'You hit the rate limit', 'error': '429'}), 429)
+
 
 def ratelimit(limit, per=300, send_x_headers=True,
               over_limit=on_over_limit,
@@ -51,9 +51,6 @@ def ratelimit(limit, per=300, send_x_headers=True,
     return decorator
 
 
-
-
-
 @app.after_request
 def inject_x_rate_headers(response):
     limit = get_view_rate_limit()
@@ -64,12 +61,14 @@ def inject_x_rate_headers(response):
         h.add('X-RateLimit-Reset', str(limit.reset))
     return response
 
+
 @app.route('/rate-limited')
 @ratelimit(limit=300, per=30 * 1)
 def index():
-    return jsonify({'response':'This is a rate limited response'})
+    return jsonify({'response': 'This is a rate limited response'})
+
 
 if __name__ == '__main__':
-	app.secret_key = 'super_secret_key'
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
