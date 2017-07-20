e65492e385b43a4145ce5cd7a7607979586ee45e
luke <luke.bei.2015@gmail.com>
2017-07-20 17:17:56 +0800

    [style]添加了注释,方便理解

--- Lesson_4/12_Rate Limiting/views.py
+++ Lesson_4/12_Rate Limiting/views.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 
 
 class RateLimit(object):
-    expiration_window = 10
+    expiration_window = 10 #键在redis中额外存活的10秒
 
     def __init__(self, key_prefix, limit, per, send_x_headers):
-        self.reset = (int(time.time()) // per) * per + per
+        self.reset = (int(time.time()) // per) * per + per #初始的时间戳
         self.key = key_prefix + str(self.reset)
         self.limit = limit
         self.per = per
         self.send_x_headers = send_x_headers
         p = redis.pipeline()
         p.incr(self.key)
-        p.expireat(self.key, self.reset + self.expiration_window)
-        self.current = min(p.execute()[0], limit)
+        p.expireat(self.key, self.reset + self.expiration_window) # 在键per时间存活的之外，额外的增加的时间是为了这样防止客户端与服务端两者之间时钟之间的同步出现问题导致redis出现问题
+        self.current = min(p.execute()[0], limit) #如果超过limit返回的就是limit
 
-    remaining = property(lambda x: x.limit - x.current)
-    over_limit = property(lambda x: x.current >= x.limit)
+    remaining = property(lambda x: x.limit - x.current) #返回剩余的请求余量，结果等于0的时候就是超过了
+    over_limit = property(lambda x: x.current >= x.limit) #如果超过了限制就会返回true
 
 
 def get_view_rate_limit():
@@ -36,16 +36,20 @@ def on_over_limit(limit):
 
 
 def ratelimit(limit, per=300, send_x_headers=True,
-              over_limit=on_over_limit,
+              over_limit=on_over_limit, #下文的onver_limit,为什么不用lambda的方式
               scope_func=lambda: request.remote_addr,
               key_func=lambda: request.endpoint):
+    '''
+    scope_func can get user ip
+    key_func can get request URL
+    '''
     def decorator(f):
         def rate_limited(*args, **kwargs):
             key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
             rlimit = RateLimit(key, limit, per, send_x_headers)
             g._view_rate_limit = rlimit
             if over_limit is not None and rlimit.over_limit:
-                return over_limit(rlimit)
+                return over_limit(rlimit)   #如果突破了限制就返回错误信息，否则继续运行装饰过的f函数
             return f(*args, **kwargs)
         return update_wrapper(rate_limited, f)
     return decorator
@@ -54,7 +58,7 @@ def ratelimit(limit, per=300, send_x_headers=True,
 @app.after_request
 def inject_x_rate_headers(response):
     limit = get_view_rate_limit()
-    if limit and limit.send_x_headers:
+    if limit and limit.send_x_headers: #limit.send_x_headers是一个布尔值
         h = response.headers
         h.add('X-RateLimit-Remaining', str(limit.remaining))
         h.add('X-RateLimit-Limit', str(limit.limit))
